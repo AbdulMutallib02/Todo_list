6af16c527642a514c993866c0ed52188435e2802
Mutallib <qmutallib2002@gmail.com>
2022-09-10 20:26:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
-function App() {
+const App = () => {
+
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] = useState([])
+
+    const itemEvents = (event) => {
+        setInputList(event.target.value)
+    }
+
+    const listOfItems = () => {
+        setItems((oldItems) => {
+            return [...oldItems, inputList];
+        });
+        setInputList("");
+    }
+
+    const deleteItems = (id) => {
+        setItems((oldItems) => {
+            return oldItems.filter((arrElem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main_div">
+        <div className="center_div">
+            <br />
+            <h2>ToDo List</h2>
+            <br />
+            <input type="text" placeholder='Add a Items' onChange={itemEvents} value={inputList} />
+            <button onClick={listOfItems}> + </button>
+            <ol>
+                {items.map((itemval, index) => {
+                    return <ToDoList
+                    key={index}
+                    id={index}
+                    text={itemval}
+                    onSelect={deleteItems}
+                    />;
+                })}
+            </ol>
+        </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
